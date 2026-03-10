eab7c5b25466ca9942f101874c848d402a2ce59e
ebosebastien68-maker <ebosebastien68@gmail.com>
2026-03-10 11:47:40 +0100

    Fix service worker registration syntax

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'; // pas d'extension
-import './styles.css'; // si tu as du CSS global
 import supabaseClient from './lib/supabaseClient'; // initialise le client côté client
 import { registerServiceWorker } from './registerServiceWorker';
 
